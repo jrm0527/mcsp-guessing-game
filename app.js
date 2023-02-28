662ea0423b8a8d482de9d5a92ac3ba4cb2ac3e0e
jrm0527 <geeziegames@gmail.com>
2023-02-28 17:48:17 -0500

    Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
-console.log('working')
+const secretNumber = Math.ceil(Math.random() * 100);
+let user = prompt("Please enter your name");
+let secondUser = null;
+let correctGuess = false;
+let numberOfGuesses = 0;
+let guesses = [];
 
-alert("Here's an alert")
+// function startNewGame() {
+//   user = prompt("Please enter your name");
+//   correctGuess = false;
+//   numberOfGuesses = 0;
+//   guesses = [];
+// }
 
-let favoriteColor = prompt("what is your favorite color?")
-console.log(favoriteColor);
+function checkGuess(guess) {
+  if (guess === secretNumber) {
+    correctGuess = true;
+    if (numberOfGuesses === 1) {
+      alert(`Correct ${user}! It only took you ${numberOfGuesses} try!`);
+    } else {
+      alert(`Correct ${user}! It only took you ${numberOfGuesses} tries!`);
+    }
+    alert(`Your guesses were ${guesses.join(", ")}.`);
+    //playAgain();
+  } else if (guess > secretNumber) {
+    alert(`Sorry ${user}, Guess Lower`);
+  } else if (guess < secretNumber) {
+    alert(`Sorry ${user}, Guess Higher`);
+  } else {
+    alert("ERROR: Please input a number");
+  }
+}
+
+while (!correctGuess) {
+  let userGuess = Number(prompt("Guess a number between 1-100."));
+  console.log(userGuess);
+  numberOfGuesses++;
+  guesses.push(userGuess);
+  checkGuess(userGuess);
+}
+
+// function playAgain() {
+//   let ans = prompt("Would you like to play again?");
+//   if (ans) {
+//     startNewGame();
+//   }
+// }

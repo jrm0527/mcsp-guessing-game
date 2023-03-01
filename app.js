2049fa2b934b956ba84a615c934eb81b3a9af417
jrm0527 <geeziegames@gmail.com>
2023-03-01 12:24:40 -0500

    refactored project

--- app.js
+++ app.js
@@ -1,87 +1,181 @@
-let secretNumber = Math.ceil(Math.random() * 100);
-console.log(secretNumber);
-let user = prompt("Please enter your name");
-let users = {};
-users[user] = 0;
-let pastUser = false;
-let correctGuess = false;
+let secretNumber = 0;
+let player = "";
+let allPlayers = {};
+let pastPlayer = false;
+let correctGuess = true;
 let numberOfGuesses = 0;
 let guesses = [];
 
+let correctMultTries = `Correct ${player}! It took you ${numberOfGuesses} tries.`;
+
+startNewGame();
+
+//Continues running until guess is correct
+do {
+  let playerGuess = Number(prompt("Guess a number between 1-100."));
+  numberOfGuesses++;
+  guesses.push(playerGuess);
+  checkGuess(playerGuess);
+} while (!correctGuess);
+
+//Starts new game
 function startNewGame() {
+  if (!player) {
+    player = prompt("Please enter your name");
+  }
+  pastPlayer = checkPlayer();
+  reset();
+}
+
+//Checks to see if player has played before
+function checkPlayer() {
+  if (allPlayers.hasOwnProperty(player)) {
+    return true;
+  }
+  allPlayers[player] = 0;
+  return false;
+}
+
+//Resets the game to start again
+function reset() {
   secretNumber = Math.ceil(Math.random() * 100);
   console.log(secretNumber);
-  user = prompt("Please enter your name");
   correctGuess = false;
   numberOfGuesses = 0;
   guesses = [];
-  checkUser(user);
 }
 
+//Checks the players guess
 function checkGuess(guess) {
   if (guess === secretNumber) {
-    correctGuess = true;
-    winningMessage();
-    users[user] = numberOfGuesses;
-    alert(`Your guesses were ${guesses.join(", ")}.`);
-    playAgain();
+    winner();
   } else if (guess > secretNumber) {
-    alert(`Sorry ${user}, Guess Lower`);
+    alert(`Sorry ${player}, Guess Lower`);
   } else if (guess < secretNumber) {
-    alert(`Sorry ${user}, Guess Higher`);
+    alert(`Sorry ${player}, Guess Higher`);
   } else {
     alert("ERROR: Please input a number");
   }
 }
 
-while (!correctGuess) {
-  let userGuess = Number(prompt("Guess a number between 1-100."));
-  numberOfGuesses++;
-  guesses.push(userGuess);
-  checkGuess(userGuess);
+//Player guesses correct number
+function winner() {
+  correctGuess = true;
+  winningMessage();
+  allPlayers[player] = numberOfGuesses;
+  alert(`Your guesses were ${guesses.join(", ")}.`);
+  playAgain();
 }
 
+//Prompts the player if they want to play again
 function playAgain() {
   let ans = prompt("Would you like to play again? Type Yes or No.");
-  if (ans == "Yes") {
-    startNewGame();
-  } else if (ans == "No") {
+  if (ans.toLowerCase() === "yes") {
+    isLastPlayer();
+  } else if (ans.toLowerCase() === "no") {
+    alert("Thanks for playing!");
     return;
   } else {
-    console.log(ans);
     alert("Please type Yes or No");
     playAgain();
   }
 }
 
-function checkUser(name) {
-  if (users.hasOwnProperty(name)) {
-    console.log("Played before");
-    pastUser = true;
+//Check if player is the same
+function isLastPlayer() {
+  let ans = prompt(`Are you ${player}? Please type Yes or No.`);
+  if (ans.toLowerCase() === "yes") {
+    startNewGame();
+  } else if (ans.toLowerCase() === "no") {
+    player = "";
+    startNewGame();
   } else {
-    console.log("First time playing");
-    users[name] = 0;
+    alert("Please type Yes or No");
+    isLastPlayer();
   }
 }
 
+//Different messages for different winning conditions
 function winningMessage() {
-  if (!pastUser) {
+  if (!pastPlayer) {
     if (numberOfGuesses === 1) {
-      alert(`Correct ${user}! It only took you ${numberOfGuesses} try!`);
+      alert(`Correct ${player}! It took you 1 try.`);
     } else {
-      alert(`Correct ${user}! It only took you ${numberOfGuesses} tries!`);
+      alert(`Correct ${player}! It took you ${numberOfGuesses} tries.`);
     }
   } else {
-    if (users[user] < numberOfGuesses) {
-      let diff = numberOfGuesses - users[user];
-      alert(
-        `Correct ${user}! You did better in your last game by ${diff} guesses.`
-      );
-    } else if (users[user] > numberOfGuesses) {
-      let diff = users[user] - numberOfGuesses;
-      alert(`Correct ${user}! You beat your last game by ${diff} guesses.`);
+    if (allPlayers[player] < numberOfGuesses) {
+      betterLastGame();
+    } else if (allPlayers[player] > numberOfGuesses) {
+      beatLastGame();
     } else {
-      alert(`Correct ${user}! You did the same as your last game.`);
+      if (numberOfGuesses === 1) {
+        alert(
+          `Correct ${player}! It took you 1 try. You did the same as your last game.`
+        );
+      } else {
+        alert(
+          `Correct ${player}! It took you ${numberOfGuesses} tries. You did the same as your last game.`
+        );
+      }
+    }
+  }
+}
+
+//Messages for a better previous game
+function betterLastGame() {
+  let diff = numberOfGuesses - allPlayers[player];
+  if (diff === 1) {
+    if (numberOfGuesses === 1) {
+      alert(`${correctOneTry} You did better in your last game by 1 guess.`);
+      // This line is probably not needed
+      console.log(
+        "It actually hit numberOfGuesses === 1 but still did better last game."
+      );
+    } else {
+      alert(
+        `Correct ${player}! It took you ${numberOfGuesses} tries. You did better in your last game by 1 guess.`
+      );
+    }
+  } else {
+    if (numberOfGuesses === 1) {
+      alert(
+        `Correct ${player}! It took you 1 try. You did better in your last game by ${diff} guesses.`
+      );
+      // This line is probably not needed
+      console.log(
+        "It actually hit numberOfGuesses === 1 but still did better last game."
+      );
+    } else {
+      alert(
+        `Correct ${player}! It took you ${numberOfGuesses} tries. You did better in your last game by ${diff} guesses.`
+      );
+    }
+  }
+}
+
+//Messages for beating last game
+function beatLastGame() {
+  let diff = allPlayers[player] - numberOfGuesses;
+  if (diff === 1) {
+    if (numberOfGuesses === 1) {
+      alert(
+        `Correct ${player}! It took you 1 try. You beat your last game by 1 guess.`
+      );
+    } else {
+      alert(
+        `Correct ${player}! It took you ${numberOfGuesses} tries. You beat your last game by 1 guess.`
+      );
+    }
+  } else {
+    if (numberOfGuesses === 1) {
+      alert(
+        `Correct ${player}! It took you 1 try. You beat your last game by ${diff} guesses.`
+      );
+    } else {
+      alert(
+        `Correct ${player}! It took you ${numberOfGuesses} tries. You beat your last game by ${diff} guesses.`
+      );
     }
   }
 }
